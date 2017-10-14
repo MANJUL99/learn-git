4585b0e4dbdc68c187323818e866b72d5af5de78
MANJUL99 <manjulsharma548@gmail.com>
2017-10-14 21:52:53 +0530

    i know 'what to do 'with that
    Signed-off-by: MANJUL99 <manjulsharma548@gmail.com>

--- ss.js
+++ ss.js
@@ -3,5 +3,8 @@ function greet(name)
 {
     console.log("KNock Knock Knock "+name);
     console.log("KNock Knock Knock "+name);
+    console.log("how did u do this?");
+    console.log("what u want from me bloody shit??");
+    console.log("what u want from me again??");
 }
 greet(Manjul);
